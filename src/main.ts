6e9c99f2b9082e8f17d179d97337360abfc4c302
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2025-06-18 00:08:54 +0200

    Moved to using bootstrap for Formly because the ionic plugin has a known problem with Select fields.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { importProvidersFrom } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
+
 import { PreloadAllModules, RouteReuseStrategy, provideRouter, withPreloading } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
-import { withFormlyIonic } from '@ngx-formly/ionic';
+
+import { withFormlyBootstrap } from '@ngx-formly/bootstrap';
 import { provideFormlyCore } from '@ngx-formly/core';
 import { provideMarkdown } from 'ngx-markdown';
 
@@ -19,6 +21,7 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withPreloading(PreloadAllModules)),
     provideAnimations(),
     provideMarkdown(),
-    provideFormlyCore(withFormlyIonic()),
+
+    provideFormlyCore(withFormlyBootstrap()),
   ],
 });
